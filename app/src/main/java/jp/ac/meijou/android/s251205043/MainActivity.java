85e4e3098eb8f53d51f960b62253d62dfb5a96f2
Reina Kakehi <251205043@ccmailg.meijo-u.ac.jp>
2026-09-01 14:31:46 +0900

    DataStoreへの保存の演習 ２日目３時間目

--- app/src/main/java/jp/ac/meijou/android/s251205043/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205043/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.meijou.android.s251205043;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +16,7 @@ import jp.ac.meijou.android.s251205043.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +26,44 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        prefDataStore = PrefDataStore.getInstance(this);
 
         //TextView textView = findViewById(R.id.text_view);
         //textView.setText(R.string.app_name);
+        //binding.textView.setText(R.string.app_name);
 
-        binding.textView.setText(R.string.app_name);
+        binding.button.setOnClickListener(view -> {
+            String text = binding.editTextText.getText().toString();
+            binding.textView.setText(text);
+        });
 
+        binding.savebutton.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String text = editable.toString();
+                binding.textView.setText(text);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+        });
     }
 }
