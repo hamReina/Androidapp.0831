7e1085d18567dbbc027602492f16be948f27070c
Reina Kakehi <251205043@ccmailg.meijo-u.ac.jp>
2026-09-02 15:04:08 +0900

    画面遷移 ２日目3時間目

--- app/src/main/java/jp/ac/meijou/android/s251205043/MainActivity3.java
+++ app/src/main/java/jp/ac/meijou/android/s251205043/MainActivity3.java
@@ -1,5 +1,6 @@
 package jp.ac.meijou.android.s251205043;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity3 extends AppCompatActivity {
+import jp.ac.meijou.android.s251205043.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s251205043.databinding.ActivityMain3Binding;
 
+public class MainActivity3 extends AppCompatActivity {
+    
+    private ActivityMain3Binding binding;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main3);
+        binding= ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main3);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        
+        Intent intent=getIntent();
+        String sentText = intent.getStringExtra("editTet");
+        binding.textViewInput.setText(sentText);
+        
+        //電卓用コード
+        binding.button0.setOnClickListener(view->{
+            
         });
     }
 }
